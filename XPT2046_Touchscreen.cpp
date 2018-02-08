8559ec10e0cd862c6be0cc057056a2b1675c4bc8
Timo Meyer <timo.git@mirvr.com>
2018-02-08 03:42:58 +0100

    Reformatted everything. Added optional calibration with EEPROM save method, moved rotation-method to get unrotated raw-data for calibration. Added Calibration-example and calibrated draw example.

--- XPT2046_Touchscreen.cpp
+++ XPT2046_Touchscreen.cpp
@@ -1,24 +1,24 @@
 /* Touchscreen library for XPT2046 Touch Controller Chip
- * Copyright (c) 2015, Paul Stoffregen, paul@pjrc.com
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice, development funding notice, and this permission
- * notice shall be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+   Copyright (c) 2015, Paul Stoffregen, paul@pjrc.com
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice, development funding notice, and this permission
+   notice shall be included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+   THE SOFTWARE.
+*/
 
 #include "XPT2046_Touchscreen.h"
 
@@ -30,53 +30,66 @@
 static XPT2046_Touchscreen 	*isrPinptr;
 void isrPin(void);
 
-bool XPT2046_Touchscreen::begin()
+bool XPT2046_Touchscreen::begin(uint16_t width, uint16_t height, TS_Calibration cal)
 {
-	SPI.begin();
-	pinMode(csPin, OUTPUT);
-	digitalWrite(csPin, HIGH);
-	if (255 != tirqPin) {
-		pinMode( tirqPin, INPUT );
-		attachInterrupt(digitalPinToInterrupt(tirqPin), isrPin, FALLING);
-		isrPinptr = this;
-	}
-	return true;
+  SPI.begin();
+  pinMode(csPin, OUTPUT);
+  digitalWrite(csPin, HIGH);
+  if (255 != tirqPin) {
+    pinMode( tirqPin, INPUT );
+    attachInterrupt(digitalPinToInterrupt(tirqPin), isrPin, FALLING);
+    isrPinptr = this;
+  }
+
+  _width = width;
+  _height = height;
+
+  this->setCalibration(cal);
+  return true;
 }
 
 void isrPin( void )
 {
-	XPT2046_Touchscreen *o = isrPinptr;
-	o->isrWake = true;
+  XPT2046_Touchscreen *o = isrPinptr;
+  o->isrWake = true;
 }
 
 TS_Point XPT2046_Touchscreen::getPoint()
 {
-	update();
-	return TS_Point(xraw, yraw, zraw);
+  update();
+
+  uint16_t x = xraw, y = yraw;
+
+  this->rotate(x, y);
+
+  return TS_Point(x, y, zraw);
 }
 
 bool XPT2046_Touchscreen::tirqTouched()
 {
-	return (isrWake);
+  return (isrWake);
 }
 
 bool XPT2046_Touchscreen::touched()
 {
-	update();
-	return (zraw >= Z_THRESHOLD);
+  update();
+  return (zraw >= Z_THRESHOLD);
 }
 
 void XPT2046_Touchscreen::readData(uint16_t *x, uint16_t *y, uint8_t *z)
 {
-	update();
-	*x = xraw;
-	*y = yraw;
-	*z = zraw;
+  update();
+
+  *x = xraw;
+  *y = yraw;
+  *z = zraw;
+
+  this->rotate(*x, *y);
 }
 
 bool XPT2046_Touchscreen::bufferEmpty()
 {
-	return ((millis() - msraw) < MSEC_THRESHOLD);
+  return ((millis() - msraw) < MSEC_THRESHOLD);
 }
 
 static int16_t besttwoavg( int16_t x , int16_t y , int16_t z ) {
@@ -98,73 +111,121 @@ static int16_t besttwoavg( int16_t x , int16_t y , int16_t z ) {
 
 void XPT2046_Touchscreen::update()
 {
-	int16_t data[6];
+  int16_t data[6];
 
-	if (!isrWake) return;
-	uint32_t now = millis();
-	if (now - msraw < MSEC_THRESHOLD) return;
-	
-	SPI.beginTransaction(SPI_SETTING);
-	digitalWrite(csPin, LOW);
-	SPI.transfer(0xB1 /* Z1 */);
-	int16_t z1 = SPI.transfer16(0xC1 /* Z2 */) >> 3;
-	int z = z1 + 4095;
-	int16_t z2 = SPI.transfer16(0x91 /* X */) >> 3;
-	z -= z2;
-	if (z >= Z_THRESHOLD) {
-		SPI.transfer16(0x91 /* X */);  // dummy X measure, 1st is always noisy
-		data[0] = SPI.transfer16(0xD1 /* Y */) >> 3;
-		data[1] = SPI.transfer16(0x91 /* X */) >> 3; // make 3 x-y measurements
-		data[2] = SPI.transfer16(0xD1 /* Y */) >> 3;
-		data[3] = SPI.transfer16(0x91 /* X */) >> 3;
-	}
-	else data[0] = data[1] = data[2] = data[3] = 0;	// Compiler warns these values may be used unset on early exit.
-	data[4] = SPI.transfer16(0xD0 /* Y */) >> 3;	// Last Y touch power down
-	data[5] = SPI.transfer16(0) >> 3;
-	digitalWrite(csPin, HIGH);
-	SPI.endTransaction();
-	//Serial.printf("z=%d  ::  z1=%d,  z2=%d  ", z, z1, z2);
-	if (z < 0) z = 0;
-	if (z < Z_THRESHOLD) { //	if ( !touched ) {
-		// Serial.println();
-		zraw = 0;
-		if (z < Z_THRESHOLD_INT) { //	if ( !touched ) {
-			if (255 != tirqPin) isrWake = false;
-		}
-		return;
-	}
-	zraw = z;
-	
-	// Average pair with least distance between each measured x then y
-	//Serial.printf("    z1=%d,z2=%d  ", z1, z2);
-	//Serial.printf("p=%d,  %d,%d  %d,%d  %d,%d", zraw,
-		//data[0], data[1], data[2], data[3], data[4], data[5]);
-	int16_t x = besttwoavg( data[0], data[2], data[4] );
-	int16_t y = besttwoavg( data[1], data[3], data[5] );
-	
-	//Serial.printf("    %d,%d", x, y);
-	//Serial.println();
-	if (z >= Z_THRESHOLD) {
-		msraw = now;	// good read completed, set wait
-		switch (rotation) {
-		  case 0:
-			xraw = 4095 - y;
-			yraw = x;
-			break;
-		  case 1:
-			xraw = x;
-			yraw = y;
-			break;
-		  case 2:
-			xraw = y;
-			yraw = 4095 - x;
-			break;
-		  default: // 3
-			xraw = 4095 - x;
-			yraw = 4095 - y;
-		}
-	}
+  if (!isrWake) return;
+  uint32_t now = millis();
+  if (now - msraw < MSEC_THRESHOLD) return;
+
+  SPI.beginTransaction(SPI_SETTING);
+  digitalWrite(csPin, LOW);
+  SPI.transfer(0xB1 /* Z1 */);
+  int16_t z1 = SPI.transfer16(0xC1 /* Z2 */) >> 3;
+  int z = z1 + 4095;
+  int16_t z2 = SPI.transfer16(0x91 /* X */) >> 3;
+  z -= z2;
+  if (z >= Z_THRESHOLD) {
+    SPI.transfer16(0x91 /* X */);  // dummy X measure, 1st is always noisy
+    data[0] = SPI.transfer16(0xD1 /* Y */) >> 3;
+    data[1] = SPI.transfer16(0x91 /* X */) >> 3; // make 3 x-y measurements
+    data[2] = SPI.transfer16(0xD1 /* Y */) >> 3;
+    data[3] = SPI.transfer16(0x91 /* X */) >> 3;
+  }
+  else data[0] = data[1] = data[2] = data[3] = 0;	// Compiler warns these values may be used unset on early exit.
+  data[4] = SPI.transfer16(0xD0 /* Y */) >> 3;	// Last Y touch power down
+  data[5] = SPI.transfer16(0) >> 3;
+  digitalWrite(csPin, HIGH);
+  SPI.endTransaction();
+  //Serial.printf("z=%d  ::  z1=%d,  z2=%d  ", z, z1, z2);
+  if (z < 0) z = 0;
+  if (z < Z_THRESHOLD) { //	if ( !touched ) {
+    // Serial.println();
+    zraw = 0;
+    if (z < Z_THRESHOLD_INT) { //	if ( !touched ) {
+      if (255 != tirqPin) isrWake = false;
+    }
+    return;
+  }
+  zraw = z;
+
+  // Average pair with least distance between each measured x then y
+  //Serial.printf("    z1=%d,z2=%d  ", z1, z2);
+  //Serial.printf("p=%d,  %d,%d  %d,%d  %d,%d", zraw,
+  //data[0], data[1], data[2], data[3], data[4], data[5]);
+  int16_t x = besttwoavg( data[0], data[2], data[4] );
+  int16_t y = besttwoavg( data[1], data[3], data[5] );
+
+  //Serial.printf("    %d,%d", x, y);
+  //Serial.println();
+  if (z >= Z_THRESHOLD) {
+    msraw = now;	// good read completed, set wait
+    xraw = x;
+    yraw = y;
+  }
 }
 
+TS_Point XPT2046_Touchscreen::getPixel() {
+  update();
+  
+#if defined(SWAP_AXES) && SWAP_AXES
+  uint16_t xPixel = (uint16_t)(_cal_dx * (yraw - _cal_vj1) / _cal_dvj + CAL_OFFSET);
+  uint16_t yPixel = (uint16_t)(_cal_dy * (xraw - _cal_vi1) / _cal_dvi + CAL_OFFSET);
+#else
+  uint16_t xPixel = (uint16_t)(_cal_dx * (xraw - _cal_vi1) / _cal_dvi + CAL_OFFSET);
+  uint16_t yPixel = (uint16_t)(_cal_dy * (yraw - _cal_vj1) / _cal_dvj + CAL_OFFSET);
+#endif
+  
+  this->rotate(xPixel, yPixel);
 
+  return TS_Point(xPixel, yPixel, zraw);
+}
+
+void XPT2046_Touchscreen::getCalibrationPoints(uint16_t &x1, uint16_t &y1, uint16_t &x2, uint16_t &y2) {
+  x1 = y1 = CAL_OFFSET;
+  x2 = _width - CAL_OFFSET;
+  y2 = _height - CAL_OFFSET;
+}
+
+void XPT2046_Touchscreen::setCalibration (TS_Calibration cal) {
+  _cal_dx = _width - 2 * CAL_OFFSET;
+  _cal_dy = _height - 2 * CAL_OFFSET;
+
+  _cal_vi1 = (int32_t)cal.vi1;
+  _cal_vj1 = (int32_t)cal.vj1;
+  _cal_dvi = (int32_t)cal.vi2 - cal.vi1;
+  _cal_dvj = (int32_t)cal.vj2 - cal.vj1;
+}
+
+void XPT2046_Touchscreen::saveCalibrationToEEPROM(TS_Calibration &cal, unsigned int eepromSlot){
+  eepromSlot = eepromSlot * sizeof(cal);
+  
+  EEPROM.put(eepromSlot, cal);
+}
+
+TS_Calibration XPT2046_Touchscreen::getEEPROMCalibration(unsigned int eepromSlot){
+  eepromSlot = eepromSlot * sizeof(TS_Calibration);
+  TS_Calibration cal;
+
+  EEPROM.get(eepromSlot, cal);
+
+  return cal;
+}
+
+void XPT2046_Touchscreen::rotate(uint16_t &x, uint16_t &y){
+  switch (rotation) {
+    case 0:
+      x = 4095 - y;
+      y = x;
+      break;
+    case 1:
+      break;
+    case 2:
+      x = y;
+      y = 4095 - x;
+      break;
+    default: // 3
+      x = 4095 - x;
+      y = 4095 - y;
+  }
+}
 
---
+++ examples/DrawWithCalibration/DrawWithCalibration.ino
@@ -0,0 +1,50 @@
+#include "XPT2046_Touchscreen.h"
+#include <ILI9341_t3.h>
+
+// Modify the following lines to match your hardware
+#define TFT_DC   15
+#define TFT_CS   10
+#define TFT_RST  4
+//#define TFT_MOSI 11
+//#define TFT_MISO 12
+//#define TFT_CLK  13
+
+//Some Displays need an extra Pin for the LED-Backlight. I used a I/O-Pin also to dim the Backlight.
+#define TFT_LED  19
+
+//And also the Touch-Pins for the XPT2046
+#define TOUCH_CS  20
+#define TOUCH_IRQ 21
+
+#define BLACK   0x0000
+#define WHITE   0xFFFF
+
+ILI9341_t3 display = ILI9341_t3(TFT_CS, TFT_DC, TFT_RST);
+//ILI9341_t3 display = ILI9341_t3(TFT_CS, TFT_DC, TFT_RST, TFT_MOSI, TFT_CLK, TFT_MISO);
+
+XPT2046_Touchscreen touch(TOUCH_CS, TOUCH_IRQ);
+
+void setup() {
+  //Activate backlight
+  pinMode(TFT_LED, OUTPUT);
+  digitalWrite(TFT_LED, HIGH);
+
+  delay(1000);
+
+  //Start display and touch
+  display.begin();
+  touch.begin(display.width(), display.height(), touch.getEEPROMCalibration());
+
+  //Draw background
+  display.fillScreen(BLACK);
+}
+
+void loop() {
+  //If touched than draw
+  if (touch.touched()) {
+    //Get touched and calibrated position
+    TS_Point p = touch.getPixel();
+
+    display.fillCircle(p.x, p.y, 2, WHITE);
+  }
+}
